188e01c6e4e2109937e852cac4b34bb00171295e
RuiPinto96274 <101046649+RuiPinto96274@users.noreply.github.com>
2023-12-31 11:43:38 +0000

    fix: fixed form values restrictions

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6b764db0a1340d8457c607efbde9a0196f100d0d43786650e7bbb98a797a2f2
-size 2021
+oid sha256:9c2a15dc2fb1d3a4a55aff8d498e4f8ba0ac3f8c81e10087be1382f6111e4454
+size 2074
--- src/App.js
+++ src/App.js
@@ -1,141 +1,171 @@
 import "./App.css";
 import React, { useState } from "react";
-import axios from 'axios';
+import { useForm } from "@mantine/form";
+import { Box, Button, NumberInput } from "@mantine/core";
+import axios from "axios";
 
 function App() {
-  const [formValues, setFormValues] = useState({
-    ph: "",
-    hardness: "",
-    solids: "",
-    chloramines: "",
-    sulfate: "",
-    conductivity: "",
-    organic_carbon: "",
-    trihalomethanes: "",
-    turbidity: "",
+  const form = useForm({
+    initialValues: {
+      ph: "8",
+      hardness: "200",
+      solids: "8700",
+      chloramines: "8",
+      sulfate: "350",
+      conductivity: "338",
+      organic_carbon: "18",
+      trihalomethanes: "105",
+      turbidity: "3",
+    },
+    validate: {
+      ph: (value) => (value >= 0 && value <= 14 && value !== "" ? null : "ph must be between 0 and 14"),
+      hardness: (value) => (value >= 47 && value <= 320 && value !== "" ? null : "hardness must be between 47 and 320 mg/L"),
+      solids: (value) => (value > 720 && value <= 61220 && value !== "" ? null : "solids must be between 720 and 61220 ppm"),
+      chloramines: (value) => (value >= 0 && value <= 13 && value !== "" ? null : "chloramines must be between 0 and 13 ppm"),
+      sulfate: (value) => (value >= 180 && value <= 481 && value !== "" ? null : "sulfate must be between 180 and 481 mg/L"),
+      conductivity: (value) => (value >= 210 && value <= 753 && value !== "" ? null : "conductivity must be between 210 and 753 μS/cm"),
+      organic_carbon: (value) => (value >= 2 && value <= 28 && value !== "" ? null : "organic carbon must be between 2 and 28 mg/L"),
+      trihalomethanes: (value) => (value >= 8 && value <= 124 && value !== "" ? null : "trihalomethanes must be between 8 and 124 ppm"),
+      turbidity: (value) => (value >= 1 && value <= 7 && value !== "" ? null : "turbidity must be between 1 and 7 NTU"),
+    }
   });
 
   const [predictionResult, setPredictionResult] = useState(null);
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
-  };
-
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    try {
-      const response = await axios.post("http://127.0.0.1:5000/predict", formValues, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }});
+    form.validate();
+    if (!form.isValid()) {
+      console.error('Form is not valid');
+      return;
+    } else {
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:5000/predict",
+          {...form.values},
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json",
+            },
+          }
+        );
         setPredictionResult(response.data.prediction);
-    } catch (error) {
-      console.error("Error submitting form", error);
+      } catch (error) {
+        console.error("Error submitting form", error);
+      }
     }
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <label>
-            pH:
-            <input
-              type="number"
-              name="ph"
-              value={formValues.ph}
-              onChange={handleInputChange}
+        <Box sx={{ maxWidth: 300 }} mx="auto">
+          <form onSubmit={form.onSubmit(console.log)}>
+            <NumberInput
+              label="ph"
+              placeholder="ph"
+              min={0}
+              max={14}
+              decimalScale={2}
+              hideControls
+              required
+              {...form.getInputProps("ph")}
             />
-          </label>
-          <br />
-          <label>
-            hardness:
-            <input
-              type="number"
-              name="hardness"
-              value={formValues.hardness}
-              onChange={handleInputChange}
+            <NumberInput
+              mt="md"
+              label="hardness"
+              placeholder="hardness"
+              min={47}
+              max={320}
+              decimalScale={2}
+              hideControls
+              required
+              {...form.getInputProps("hardness")}
             />
-          </label>
-          <br />
-          <label>
-            solids:
-            <input
-              type="number"
-              name="solids"
-              value={formValues.solids}
-              onChange={handleInputChange}
+            <NumberInput
+              mt="md"
+              label="solids"
+              placeholder="solids"
+              min={720}
+              max={61220}
+              decimalScale={2}
+              hideControls
+              required
+              {...form.getInputProps("solids")}
             />
-          </label>
-          <br />
-          <label>
-            chloramines:
-            <input
-              type="number"
-              name="chloramines"
-              value={formValues.chloramines}
-              onChange={handleInputChange}
+            <NumberInput
+              mt="md"
+              label="chloramines"
+              placeholder="chloramines"
+              min={0}
+              max={13}
+              decimalScale={2}
+              hideControls
+              required
+              {...form.getInputProps("chloramines")}
             />
-          </label>
-          <br />
-          <label>
-            sulfate:
-            <input
-              type="number"
-              name="sulfate"
-              value={formValues.sulfate}
-              onChange={handleInputChange}
+            <NumberInput
+              mt="md"
+              label="sulfate"
+              placeholder="sulfate"
+              min={180}
+              max={481}
+              decimalScale={2}
+              hideControls
+              required
+              {...form.getInputProps("sulfate")}
             />
-          </label>
-          <br />
-          <label>
-            conductivity:
-            <input
-              type="number"
-              name="conductivity"
-              value={formValues.conductivity}
-              onChange={handleInputChange}
+            <NumberInput
+              mt="md"
+              label="conductivity"
+              placeholder="conductivity"
+              min={210}
+              max={753}
+              decimalScale={2}
+              hideControls
+              required
+              {...form.getInputProps("conductivity")}
             />
-          </label>
-          <br />
-          <label>
-            organic_carbon:
-            <input
-              type="number"
-              name="organic_carbon"
-              value={formValues.organic_carbon}
-              onChange={handleInputChange}
+            <NumberInput
+              mt="md"
+              label="organic carbon"
+              placeholder="organic carbon"
+              min={2}
+              max={28}
+              decimalScale={2}
+              hideControls
+              required
+              {...form.getInputProps("organic_carbon")}
             />
-          </label>
-          <br />
-          <label>
-            trihalomethanes:
-            <input
-              type="number"
-              name="trihalomethanes"
-              value={formValues.trihalomethanes}
-              onChange={handleInputChange}
+            <NumberInput
+              mt="md"
+              label="trihalomethanes"
+              placeholder="trihalomethanes"
+              min={8}
+              max={124}
+              decimalScale={2}
+              hideControls
+              required
+              {...form.getInputProps("trihalomethanes")}
             />
-          </label>
-          <br />
-          <label>
-            turbidity:
-            <input
-              type="number"
-              name="turbidity"
-              value={formValues.turbidity}
-              onChange={handleInputChange}
+            <NumberInput
+              mt="md"
+              label="turbidity"
+              placeholder="turbidity"
+              min={1}
+              max={7}
+              decimalScale={2}
+              hideControls
+              required
+              {...form.getInputProps("turbidity")}
             />
-          </label>
-          <br />
-          <button type="submit">Submit</button>
-        </form>
+            <div>
+              <Button type="submit" onClick={handleSubmit}>
+                Predict
+              </Button>
+            </div>
+          </form>
           {predictionResult !== null && (
           <div>
             {predictionResult === 1 ? (
@@ -144,7 +174,9 @@ function App() {
               <h1>Water is not safe to drink!</h1>
             )}
           </div>
-          )}
+        )}
+        </Box>
+
       </header>
     </div>
   );
